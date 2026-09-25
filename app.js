c22bc7337b1285d8f3de13d33ee758f8a53e295f
Cristian Cauê Machado Santana <caiocesarmachado2@gmail.com>
2026-09-25 20:37:54 -0300

    fix: alterei a logica da conta do valor

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ function calculartotal (itens) {
     let total = 0
 
     for (let i = 0; i <itens.length; i++) {
-        total += itens[i].preco * 1.1
-        
+        total += itens[i].preco
     }
     
     return total
