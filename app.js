a4fd69ef6ab78a1478d81796f3802cdd58a11a4b
Cristian Cauê Machado Santana <caiocesarmachado2@gmail.com>
2026-09-25 20:39:16 -0300

    docs: comentario na funcao calcular total

--- app.js
+++ app.js
@@ -8,5 +8,9 @@ function calculartotal (itens) {
         total += itens[i].preco
     }
     
+
+    // aplica desconto de fidelidade
+    // antes de retornar o valor final
+    
     return total
 }  
