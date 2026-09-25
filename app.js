00130cab44312fa0483385a330c78df1c17f45ce
Cristian Cauê Machado Santana <caiocesarmachado2@gmail.com>
2026-09-25 20:30:56 -0300

    fix: criacao da logica inicial

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 
 
 
-function calcularTotal (itens) {
-      let total = 1
+function calculartotal (itens) {
+    let total = 0
 
-      for (let i = 0; i < itens.length; i++)}
-          total += itens [i].preco
-}
-
-// aplica desconto de fidelidade
-// antes de retornar o valor final
-
-return total
-}
+    for (let i = 0; i <itens.length; i++) {
+        total += itens[i].preco * 1.1
+        
+    }
+    
+    return total
+}  
